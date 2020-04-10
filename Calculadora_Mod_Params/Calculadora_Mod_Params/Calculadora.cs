f10156a676d0e62c045dae7bd5904d84b124279b
1bertoMaia <bruno1berto@hotmail.com>
2020-04-09 21:38:42 -0300

    Calculadora com atributos params

---
+++ Calculadora_Mod_Params/Calculadora_Mod_Params/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Calculadora_Mod_Params
+{
+    class Calculadora
+    {
+        public static double Soma(params double[] numeros){
+            double soma=0;
+            foreach (int num in numeros){
+                soma += num;
+            }
+            return soma;
+        }
+    }
+}
---
+++ Calculadora_Mod_Params/Calculadora_Mod_Params/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculadora_Mod_Params
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+            double[] numeros = new double[] { 2, 4, 6 };
+            double s1 = Calculadora.Soma(numeros);
+            Console.WriteLine("Resultado = " + s1);
+
+        }
+    }
+}
